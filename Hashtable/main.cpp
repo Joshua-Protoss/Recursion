22789c665a14a0644bea14d71d3992642bd274ba
Muhammad Joshua <72489749+Joshua-Protoss@users.noreply.github.com>
2025-11-22 19:27:47 +0700

    Create main.cpp

---
+++ Hashtable/main.cpp
@@ -0,0 +1,17 @@
+#include "Hashtable.h"
+#include "Hashtable.cpp"
+#include <iostream>
+
+int main(){
+    HashTable hashTable {};
+
+    hashTable.put(1,1);
+    hashTable.put(11,10);
+    hashTable.put(21,100);
+
+    std::cout << hashTable.get(1) << ' ';
+    std::cout << hashTable.get(11) << ' ';
+    std::cout << hashTable.get(21) << ' ';
+
+    return 0;
+}

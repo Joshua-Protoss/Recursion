886e4c30151ce509b8149777c1364b215cd1b0a8
Muhammad Joshua <72489749+Joshua-Protoss@users.noreply.github.com>
2026-01-10 11:45:56 +0700

    Create main.cpp

---
+++ Overload/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Mystring.h"
+
+using namespace std;
+
+int main(){
+
+    cout << boolalpha << endl;
+    Mystring a {"frank"};
+    Mystring b {"frank"};
+    //cout << (a==b) << endl; // true
+    //cout << (a!=b) << endl; // false 
+    return 0;
+}
